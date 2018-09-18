13a08e710dcdfde66f70176cf911e03108d43015
fhapocryphal-sigma <richardlfitzhugh@gmail.com>
2018-09-17 22:16:28 -0600

    Update abstracts.jl

--- src/abstracts.jl
+++ src/abstracts.jl
@@ -1,8 +1,12 @@
+using Enums
+
 abstract type AbstractSubstance end
 abstract type AbstractPhaseState end
 abstract type AbstractMixture end
 abstract type AbstractThermoBackend end
 abstract type AbstractEquationOfState end
 abstract type AbstractProperty end
+
+@enum ThermoProp pressure=1 volume=2 temperature=3 entropy=4 internalenergy=5 enthalpy=6 helmholtzenergy=7  gibbsenergy=8 
 
 function calculate(s::AbstractSubstance, st::AbstractPhaseState, b::AbstractThermoBackend, p::AbstractProperty) end
