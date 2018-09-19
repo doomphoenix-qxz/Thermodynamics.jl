c7626ea1dab74d323993a201107457abb57a440b
fhapocryphal-sigma <richardlfitzhugh@gmail.com>
2018-09-19 14:41:56 -0600

    Update Thermodynamics.jl

--- src/Thermodynamics.jl
+++ src/Thermodynamics.jl
@@ -1,5 +1,8 @@
 module Thermodynamics
 
+include("abstracts.jl")
+include("IdealGasBackend.jl")
 
+export phasestate, spec_state!, mixture, thermocalc, thermocalc!, makethermodata, makethermoplot
 
 end # module Thermodynamics
