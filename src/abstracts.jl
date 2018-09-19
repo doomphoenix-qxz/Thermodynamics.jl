df87177e82c049a848f755afbf3b62fe37a34a1f
fhapocryphal-sigma <richardlfitzhugh@gmail.com>
2018-09-19 14:48:05 -0600

    Update abstracts.jl

--- src/abstracts.jl
+++ src/abstracts.jl
@@ -7,6 +7,7 @@ abstract type AbstractThermoBackend end
 abstract type AbstractEquationOfState end
 abstract type AbstractProperty end
 
-@enum ThermoProp pressure=1 volume=2 temperature=3 entropy=4 internalenergy=5 enthalpy=6 helmholtzenergy=7  gibbsenergy=8 
+@enum ThermoProp pressure=1 volume=2 temperature=3 entropy=4 internalenergy=5 enthalpy=6 helmholtzenergy=7  gibbsenergy=8 heatcapacity=9 vaporpressure=10 chemicalpotential=11 fugacity=12 fugacitycoefficient=13 activity=14 activitycoefficient=15
+# @enum MixingRules ...to implement later
 
 function calculate(s::AbstractSubstance, st::AbstractPhaseState, b::AbstractThermoBackend, p::AbstractProperty) end
