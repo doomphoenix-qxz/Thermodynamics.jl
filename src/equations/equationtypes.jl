b59b5209bbbaf1798fdec1769049e024eb83ba25
doomphoenix-qxz <richardlfitzhugh@gmail.com>
2018-10-10 23:36:55 -0600

    Change Equation1 to CpEquation1

--- src/equations/equationtypes.jl
+++ src/equations/equationtypes.jl
@@ -1,8 +1,8 @@
-struct equation1{T<:Number}
+struct CpEquation1{T<:Number}
     vals::Array{T,1}
 end
 
-function thermocalc(eq::equation1, Temp)
+function thermocalc(eq::CpEquation1, Temp)
     A,B,C,D,E = vals
     return A+B*Temp+C*Temp^2+D*Temp^3+E/(Temp^2)
 end
