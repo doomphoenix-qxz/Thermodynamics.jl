7b12175c0be8bd515e0a588b2de320f6516db540
doomphoenix-qxz <richardlfitzhugh@gmail.com>
2019-02-18 08:42:35 -0700

    Change PengRobinson to PengRobinsonData

--- src/backends/pengrobinsonbackend.jl
+++ src/backends/pengrobinsonbackend.jl
@@ -1,9 +1,11 @@
-module PengRobinson
+# module PengRobinson
 
 using PolynomialRoots
 
-struct PengRobinson{T<:Real}
-    pc::T
+struct PengRobinsonData{T<:Real}
+    CompoundName::String
+    MW::T
+    Pc::T
     Tc::T
     a::T
     b::T
@@ -13,32 +15,32 @@ end
 
 
 
-function α(p_::PengRobinson, T)
+function α(p_::PengRobinsonData, T)
     Tr = T/p_.Tc
     return (1+p_.κ*(1-√(Tr)))^2
 end
-function A(p_::PengRobinson, p, T)
-    return p_.a * α(p_, T) * p / (R() ^2 * T^2)
+function A(p_::PengRobinsonData, P, T)
+    return p_.a * α(p_, T) * P / (R() ^2 * T^2)
 end
-function B(p_::PengRobinson, p, T)
-    return p_.b*p/(T*R())
+function B(p_::PengRobinsonData, P, T)
+    return p_.b*P/(T*R())
 end
-function c1(p_::PengRobinson, p, T)
-    return (B(p_, p, T) - 1)
+function c1(p_::PengRobinsonData, P, T)
+    return (B(p_, P, T) - 1)
 end
-function c2(p_::PengRobinson, p, T)
-    return A(p_, p, T) - 2B(p_, p,T) - 3B(p_, p,T)^2
+function c2(p_::PengRobinsonData, P, T)
+    return A(p_, P, T) - 2B(p_, P,T) - 3B(p_, P,T)^2
 end
 function c3(p_::PengRobinson, p, T)
-    return B(p_, p,T)^3 + B(p_, p,T)^2 - A(p_, p,T)*B(p_, p,T)
+    return B(p_, P,T)^3 + B(p_, P,T)^2 - A(p_, P,T)*B(p_, P,T)
 end
 
 
-function pengRobinson(pc, Tc, ω)
+function PengRobinsonData{T}(CompoundName::String, MW::T, Pc::T, Tc::T, ω::T) where T<: AbstractFloat
     a = 0.45724 * R() ^2 * Tc^2 /pc
     b = 0.0778*R() * Tc /pc
     κ = 0.37464 + 1.54226*ω - 0.26992*ω^2
-    return pengRobinson(pc, Tc, a,b,ω,κ)
+    return PengRobinsonData(CompoundName, MW, Pc, Tc, a,b,ω,κ)
 end
 
 function get_the_real(vec::Vector{Complex{T}}) where {T<:Number}
@@ -49,12 +51,12 @@ function get_the_real(vec::Vector{Complex{T}}) where {T<:Number}
     end
 end
 
-function volume_roots(p_::PengRobinson, p, T)
-    c1_ = c1(p_, p, T)
-    c2_ = c2(p_, p, T)
-    c3_ = c3(p_, p, T)
+function volume_roots(p_::PengRobinsonData, P, T)
+    c1_ = c1(p_, P, T)
+    c2_ = c2(p_, P, T)
+    c3_ = c3(p_, P, T)
     roots_ = roots([c3_, c2_, c1_, 1.0])
     return roots_
 end
 
-end # ends module PengRobinson
+# end # ends module PengRobinson
