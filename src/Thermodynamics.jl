1a7f7ef9169eb6bcaad44a727ebed3cfab11f866
doomphoenix-qxz <richardlfitzhugh@gmail.com>
2019-02-19 16:07:39 -0700

    stuff

--- src/Thermodynamics.jl
+++ src/Thermodynamics.jl
@@ -1,9 +1,14 @@
 module Thermodynamics
 
 using ThermodynamicsBase
+using Reexport
+using NLSolve
+using CSV
 
 include("abstracts.jl")
-include("IdealGasBackend.jl")
+include("backends/idealgas.jl")
+include("backends/multiparameterhelmholtz.jl")
+include("backends/pengrobinson.jl")
 
 export phasestate, spec_state!, mixture, thermocalc, thermocalc!, makethermodata, makethermoplot
 
